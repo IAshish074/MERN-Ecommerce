16ed12dc595b2caca4d5d4969433b003b4a269aa
Ashish Kumar Mishra <amishrahsm@gmail.com>
2025-06-29 00:15:01 +0530

    admit pannel ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/layout/userLayout";
 import Home from "./pages/Home";
-import {Toaster} from 'sonner'
+import { Toaster } from "sonner";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
@@ -12,32 +12,44 @@ import CheckOut from "./components/Cart/CheckOut";
 import OrderComfirmationPage from "./pages/OrderComfirmationPage";
 import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrderPage from "./pages/MyOrderPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
+import EditProductPage from "./components/Admin/EditProductPage";
+import OrderManagement from "./components/Admin/OrderManagement";
+
 function App() {
   return (
     <BrowserRouter>
-    <Toaster position="top-right"/>
+      <Toaster position="top-right" />
       <Routes>
         {/* User layout with nested routes */}
-        <Route path="/" element={<UserLayout />} >
-
+        <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
-
           <Route path="login" element={<Login />} />
-
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<Profile />} />
           <Route path="collections/:collection" element={<CollectionPage />} />
           <Route path="product/:id" element={<ProductDetails />} />
-
-          <Route path="checkout" element={<CheckOut />}/>
-          <Route path="order-confirmation" element={<OrderComfirmationPage />}/>
-          <Route path="order/:id" element={<OrderDetailsPage />}/>
-          <Route path="/my-orders" element={<MyOrderPage />}/>
-
+          <Route path="checkout" element={<CheckOut />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderComfirmationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyOrderPage />} />
         </Route>
 
-        {/* Admin layout */}
-        <Route path="/admin" element={<div>Admin layout</div>} />
+        {/* Admin layout with nested routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="products/:id/edit" element={<EditProductPage />} />
+          <Route path="orders" element={<OrderManagement />} />{" "}
+        
+        </Route>
       </Routes>
     </BrowserRouter>
   );
